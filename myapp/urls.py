668327ade46b9b87c25aa575eb50f04dc946cfbb
amrathkumar <amrathkumar2005@gmail.com>
2025-07-22 01:15:19 +0530

    final

--- myapp/urls.py
+++ myapp/urls.py
@@ -4,6 +4,5 @@ from . import views
 urlpatterns = [
     path('items/<int:item_id>/', views.item_detail, name='item_detail'),
     path('', views.show_qr, name='show_qr'),
-    path("search-product", views.search_product, name="search_product"),
     path("add-product", views.add_product, name="add_product"),
 ]
